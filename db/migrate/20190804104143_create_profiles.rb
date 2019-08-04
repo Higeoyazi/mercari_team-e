948870a9031f191c6ae737fd102c6b5b959e649a
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-04 19:59:13 +0900

    add profiles table

--- db/migrate/20190804104143_create_profiles.rb
+++ db/migrate/20190804104143_create_profiles.rb
@@ -1,12 +1,13 @@
 class CreateProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :profiles do |t|
-      add_column :profiles, :profile, :text, null: false
-      add_column :profiles, :family_name, :string, null: false
-      add_column :profiles, :first_name, :string, null: false
-      add_column :profiles, :family_name_kana, :string, null: false
-      add_column :profiles, :first_name_kana, :string, null: false
-      add_column :profiles, :phone_number, :string, null: false
+      t.text :profile, null: false
+      t.string :family_name, null: false
+      t.string :first_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :first_name_kana, null: false 
+      t.string :phone_number, null: false, unique: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_04_101534) do
+ActiveRecord::Schema.define(version: 2019_08_04_104143) do
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "profile", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "phone_number", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +37,5 @@ ActiveRecord::Schema.define(version: 2019_08_04_101534) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
