5d5bd3c60817be3f94eaafee0e397de02ce5f94d
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-04 20:38:32 +0900

    add category_genre model

--- db/migrate/20190804113636_create_category_genres.rb
+++ db/migrate/20190804113636_create_category_genres.rb
@@ -1,7 +1,7 @@
 class CreateCategoryGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :category_genres do |t|
-
+      t.string :name, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_04_113303) do
+ActiveRecord::Schema.define(version: 2019_08_04_113636) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "prefecture", null: false
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_08_04_113303) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "category_genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
