f5852d6dae55af8b095016f7efcc1f961bba4788
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-07 18:41:08 +0900

    add routes resources signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: 
-  {registrations: 'users/registrations',
-   omniauth_callbacks: 'users/omniauth_callbacks'}
-  
+  {registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks'}
+
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4'
+      get 'step5'# ここで、入力の全てが終了する
+      get 'done' # 登録完了後のページ
+    end
+  end
+
   root 'tweets#index'
 end
