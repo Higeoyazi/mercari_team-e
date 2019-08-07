e0f1c4c54a407017917901e41c5c3df52795407e
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-07 19:04:28 +0900

    add view file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: 
-  {registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks'}
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
   resources :signup do
     collection do
