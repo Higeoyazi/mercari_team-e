8d128ae002c6d8951e6bb197f68b7f269600bcfe
tfljliar <tfljliar1101@gmail.com>
2019-08-07 21:24:30 +0900

    add product index server

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   # Association
   belongs_to :user
   has_one :order
-  has_many :images, dependent: :destroy
+  has_many :product_images, dependent: :destroy
   has_many :categories
   has_many :category_genres, through: :categories
   has_many :comments, dependent: :destroy
