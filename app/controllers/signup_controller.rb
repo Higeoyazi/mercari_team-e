a7ea79202aa7a1d906a440a19cbad41ec5941eca
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-08 15:28:25 +0900

    add refactring signup_controller

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -9,17 +9,9 @@ def step2
   @user = User.new
   @user.build_profile
   session[:user_params] = user_params
-  # @profile = Profile.new
-  # session[:nickname] = user_params[:nickname]
-  # session[:email] = user_params[:email]
-  # session[:password] = user_params[:password]
-  # session[:password_confirmation] = user_params[:password_confirmation]
-  # binding.pry
 end
 
 def step3
-  # @profile = Profile.new
-  # @address = Address.new
   @user = User.new
   @user.build_profile
   @user.build_address
@@ -29,38 +21,18 @@ end
 def step4
   @user = User.new
   @user.build_credit_card
-  # @credit_card = CreditCard.new
-
-  #session for profile
   session[:profile_attributes] = user_params[:profile_attributes] 
-  # session[:family_name] = user_params[:profile_attributes][:family_name]
-  # session[:first_name] = user_params[:profile_attributes][:first_name]
-  # session[:family_name_kana] = user_params[:profile_attributes][:family_name_kana]
-  # session[:first_name_kana] = user_params[:profile_attributes][:first_name_kana]
-
-  #session for address
   session[:address_attributes] = user_params[:address_attributes] 
-  # session[:postal_code] = user_params[:address_attributes][:postal_code]
-  # session[:prefecture] = user_params[:address_attributes][:prefecture]
-  # session[:city] = user_params[:address_attributes][:city]
-  # session[:block] = user_params[:address_attributes][:block]
-  # session[:building] = user_params[:address_attributes][:building]
 end
 
 
 def create
-  @user = User.new(session[:user_params]
-    # nickname: session[:nickname],
-    # email: session[:email],
-    # password: session[:password],
-    # password_confirmation: session[:password_confirmation],
-  )
-  
+  @user = User.new(session[:user_params])
   session[:profile_attributes][:phone_number] = session[:phone_number]
   @user.build_profile(session[:profile_attributes])
   @user.build_address(session[:address_attributes])
   @user.build_credit_card(user_params[:credit_card_attributes])
-  # binding.pry
+
   if @user.save
   # ログインするための情報を保管
     session[:id] = @user.id
@@ -74,35 +46,36 @@ def done
   sign_in User.find(session[:id]) unless user_signed_in?
 end
 
-  private
-    def user_params
-      params.require(:user).permit(
-        :nickname, 
-        :email, 
-        :password, 
-        :password_confirmation,
-        profile_attributes: [
-          :id,
-          :family_name,
-          :first_name,
-          :family_name_kana,
-          :first_name_kana,
-          :phone_number
-        ],
-        address_attributes: [
-          :id,
-          :postal_code,
-          :prefecture,
-          :city,
-          :block,
-          :building
-        ],
-        credit_card_attributes: [
-          :id,
-          :number,
-          :expiration_date_month,
-          :expiration_date_year,
-          :security_code
-        ])
-    end
+
+    private
+      def user_params
+        params.require(:user).permit(
+          :nickname, 
+          :email, 
+          :password, 
+          :password_confirmation,
+          profile_attributes: [
+            :id,
+            :family_name,
+            :first_name,
+            :family_name_kana,
+            :first_name_kana,
+            :phone_number
+          ],
+          address_attributes: [
+            :id,
+            :postal_code,
+            :prefecture,
+            :city,
+            :block,
+            :building
+          ],
+          credit_card_attributes: [
+            :id,
+            :number,
+            :expiration_date_month,
+            :expiration_date_year,
+            :security_code
+          ])
+      end
 end
