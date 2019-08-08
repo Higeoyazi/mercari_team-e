270b739e3ea5c2f9226cadcc79d11d98ab2f1b81
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-08 21:34:35 +0900

    fix signup_controller

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -7,35 +7,35 @@ class SignupController < ApplicationController
   before_action :validates_step5, only: :create
 
 def step1
-  @user = User.new
+  new_user
 end
 
 def step2
-  @user = User.new
+  new_user
   @user.build_profile
   session[:user_params] = user_params
 end
 
 def step3
-  @user = User.new
+  new_user
   @user.build_profile
   @user.build_address
   session[:phone_number] = user_params[:profile_attributes][:phone_number]
 end
 
 def step4
-  @user = User.new
+  new_user
   @user.build_credit_card
   session[:profile_attributes] = user_params[:profile_attributes]
   session[:address_attributes] = user_params[:address_attributes]
 end
 
 def validates_step1 #for User
-  @user = User.new(user_params)
+  new_user_with_params
   render '/signup/step1' unless @user.valid?(:validates_step1)
 end
 
-# def validates_step2 #for phone_number ここは個別でバリデーションをかける必要があるから後回し
+# def validates_step2 #for phone_number ここは個別でバリデーションをかける必要があるから後回し(gem入れる必要がある)
 #   @profile = Profile.new( phone_number: user_params[:profile_attributes][:phone_number])
 #   # binding.pry
 #   render '/signup/step2' if @profile.phone_number.nil? || unless @profile.phone_number.valid?
@@ -49,13 +49,17 @@ def validates_step3 #for Profile
 end
 
 def validates_step4 #for Address
-
-
+  new_user_with_params
+  @address = Address.new(user_params[:address_attributes])
+  render '/signup/step3' unless @address.valid?(:validates_step4)
 end
 
 def validates_step5 #for CreditCard
-
+  new_user_with_params
+  @credit_card = CreditCard.new(user_params[:credit_card_attributes])
+  render '/signup/step4' unless @credit_card.valid?(:validates_step4)
 end
+
 
 
 def create
@@ -66,7 +70,6 @@ def create
   @user.build_credit_card(user_params[:credit_card_attributes])
 
   if @user.save
-  # ログインするための情報を保管
     session[:id] = @user.id
     redirect_to done_signup_index_path
   else
@@ -79,35 +82,44 @@ def done
 end
 
 
-    private
-      def user_params
-        params.require(:user).permit(
-          :nickname, 
-          :email, 
-          :password, 
-          :password_confirmation,
-          profile_attributes: [
-            :id,
-            :family_name,
-            :first_name,
-            :family_name_kana,
-            :first_name_kana,
-            :phone_number
-          ],
-          address_attributes: [
-            :id,
-            :postal_code,
-            :prefecture,
-            :city,
-            :block,
-            :building
-          ],
-          credit_card_attributes: [
-            :id,
-            :number,
-            :expiration_date_month,
-            :expiration_date_year,
-            :security_code
-          ])
-      end
+  private
+
+  def new_user
+    @user = User.new
+  end
+
+  def new_user_with_params
+    @user = User.new(user_params)
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :nickname, 
+      :email, 
+      :password, 
+      :password_confirmation,
+      profile_attributes: [
+        :id,
+        :family_name,
+        :first_name,
+        :family_name_kana,
+        :first_name_kana,
+        :phone_number
+      ],
+      address_attributes: [
+        :id,
+        :postal_code,
+        :prefecture,
+        :city,
+        :block,
+        :building
+      ],
+      credit_card_attributes: [
+        :id,
+        :number,
+        :expiration_date_month,
+        :expiration_date_year,
+        :security_code
+      ])
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -4,8 +4,8 @@ class Address < ApplicationRecord
   belongs_to :user, optional: true
 
   # Validation
-  validates :postal_code, presence: true, on: :validates_step3
-  validates :prefecture,  presence: true, on: :validates_step3
-  validates :city,        presence: true, on: :validates_step3
-  validates :block,       presence: true, on: :validates_step3
+  validates :postal_code, presence: true, on: :validates_step4
+  validates :prefecture,  presence: true, on: :validates_step4
+  validates :city,        presence: true, on: :validates_step4
+  validates :block,       presence: true, on: :validates_step4
 end
