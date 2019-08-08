f7b1adf2ff132634d5e5a7f0c9b11262a3e1c811
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-08 16:19:16 +0900

    add validation to credit_card model

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -2,5 +2,10 @@ class CreditCard < ApplicationRecord
   # Association
   belongs_to :user, optional: true
 
- 
+  # Validation
+  validates :number,                presence: true,
+                                    unique: true,   on: :validates_step4
+  validates :expiration_date_month, presence: true, on: :validates_step4
+  validates :expiration_date_year,  presence: true, on: :validates_step4
+  validates :security_code,         presence: true, on: :validates_step4
 end
