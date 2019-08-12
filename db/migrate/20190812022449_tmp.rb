bd376065b1a55597966c7afac98b4e538697093d
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-12 14:34:41 +0900

    delete migration_file no need

--- db/migrate/20190812022449_tmp.rb
+++
@@ -1,5 +0,0 @@
-class Tmp < ActiveRecord::Migration
-  def change
-     remove_column :orders, :user, :references
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -53,8 +53,8 @@ ActiveRecord::Schema.define(version: 2019_08_11_110610) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "customer_id"
-    t.string "card_id"
+    t.string "customer_id", default: "", null: false
+    t.string "card_id", default: "", null: false
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
   end
 
