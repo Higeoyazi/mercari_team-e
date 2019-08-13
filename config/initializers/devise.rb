6e1d84b7546f53e3a83fae95a476e7a490411d63
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-13 16:44:37 +0900

    fix devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,12 +5,12 @@
 Devise.setup do |config|
   require 'devise/orm/active_record'
   #require "omniauth-google-oauth2"
-  # config.omniauth :google_oauth2,
-  #                 Rails.application.credentials.development[:google_client_id],
-  #                 Rails.application.credentials.development[:google_client_secret]
-  # config.omniauth :facebook,
-  #                 Rails.application.credentials.development[:facebook_client_id],
-  #                 Rails.application.credentials.development[:facebook_client_secret]
+  config.omniauth :google_oauth2,
+                  Rails.application.credentials.development[:google_client_id],
+                  Rails.application.credentials.development[:google_client_secret]
+  config.omniauth :facebook,
+                  Rails.application.credentials.development[:facebook_client_id],
+                  Rails.application.credentials.development[:facebook_client_secret]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
