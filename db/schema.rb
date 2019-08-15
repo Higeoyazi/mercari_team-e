1548798597f39e855cbad3ccc8a9c035db79381e
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-15 13:38:17 +0900

    add migration add_ancestry_to_category ancestry:string:index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_041822) do
+ActiveRecord::Schema.define(version: 2019_08_15_043622) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "prefecture", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_08_15_041822) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
