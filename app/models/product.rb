a4597d05e705f455c6282e715ad63278e7b302ee
tfljliar <tfljliar1101@gmail.com>
2019-08-16 13:16:45 +0900

    add active_hash

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,16 @@ class Product < ApplicationRecord
   has_many :categories
   accepts_nested_attributes_for :categories
   has_many :comments, dependent: :destroy
+  #ActiveHashのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :condition
+
+  validates :name, presence: true, length: {maximum: 40}
+  validates :description, presence: true, length: {maximum: 1000 }
+  validates :price, presence: true, numericality: {less_than: 999999, greater_than: 300}
+  validates :quality, presence: true
+  validates :delivery_origin, presence: true
+  validates :delivery_cost, presence: true
+  validates :prep_days, presence: true
 end
