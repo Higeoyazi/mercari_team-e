00a4d2cbbd7a69b27781e261c01c9cb59e66529f
jinugasachio <49634472+jinugasachio@users.noreply.github.com>
2019-08-18 00:45:51 +0900

    Revert "Update migrate products add saler buyer"

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,5 @@
 class Order < ApplicationRecord
   # Association
-  belongs_to :buyer, class_name: 'User', :foreign_key => 'buyer_id'
-  belongs_to :saler, class_name: 'User', :foreign_key => 'saler_id'
+  belongs_to :user
   belongs_to :product
 end
