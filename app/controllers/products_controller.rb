48a1a6cf1c462de1aff4b4c4fc76df573938f023
taka321 <takasaki1232@gmail.com>
2019-08-17 15:22:08 +0900

    add destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,6 +29,12 @@ class ProductsController < ApplicationController
     @products = Product.where('name LIKE(?)', "%#{params[:keyword]}%")
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to root_path
+  end
+
 private
   def product_params
     params.require(:product).permit(
