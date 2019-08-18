2c497dcdda2dafa11680530a9643ed2f1421320d
taka321 <takasaki1232@gmail.com>
2019-08-18 20:31:25 +0900

    add model profile test

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :profile do
-    
+    family_name         {"高崎"}
+    first_name          {"優作"}
+    family_name_kana    {"タカサキ"}
+    first_name_kana     {"ユウサク"}
+    phone_number        {"08068057646"}
+    created_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
+    updated_at{ Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
+    user_id {1}
   end
 end
