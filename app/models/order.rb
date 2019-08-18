b46297224b9ee2a8fc365bf31dc54f22be0416e4
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-18 13:44:03 +0900

    add association to order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,9 @@
 class Order < ApplicationRecord
   # Association
-  belongs_to :user
   belongs_to :product
+  belongs_to :seller, class_name: 'User'
+  belongs_to :buyer, class_name: 'User'
+
+  # Validation
+  # 全てreferences型なので書く必要なし
 end
