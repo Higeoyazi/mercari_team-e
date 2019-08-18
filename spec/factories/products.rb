827f765bfa5fff8038dd28b9476e4126cfd799d5
rui-watanabe <wataru610806@gmail.com>
2019-08-18 17:21:46 +0900

    test_product-model
    # What
    Productモデルの単体テスト
    
    # Why
    Productモデルのバリデーションにエラーがないか確認するため

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,13 +2,17 @@ FactoryBot.define do
   factory :product do
     name{"NIKE"}
     description{"NIKE"}
-    price{"100"}
+    price{500}
     size{"L"}
     brand_name{"NIKE"}
-    quality{"1"}
+    quality{1}
+    delivery_cost{"出品者"}
     delivery_origin{"東京"}
-    delivery_status{"5"}
-    user
+    delivery_status{1}
+    prep_days{"2~3日で発送"}
     created_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
+    updated_at{ Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
+    category_id {1}
+    user_id {1}
   end
 end
