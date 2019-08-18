b63ba3f333e5a13dd38d6f53d2911ca64e4ea527
tfljliar <tfljliar1101@gmail.com>
2019-08-18 17:24:04 +0900

    add product_edit function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,16 @@
 class ProductsController < ApplicationController
-
+  before_action :set_product, only: [:edit, :show, :destroy, :update]
 
   def index
     @products = Product.all.order("created_at DESC").limit(4)
   end
 
   def show
-    @product = Product.find(params[:id])
   end
   
   def new
     @product = Product.new
     @product.product_images.build
-
   end
 
   def create
@@ -25,11 +23,33 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.user_id == current_user.id
+      # binding.pry
+      @product.update(product_params)
+      redirect_to product_path(@product.id)
+    end
+  end
+
+  def destroy
+    if @product.user_id == current_user.id
+      @tweet.destroy
+    end
+  end
+
   def search
     @products = Product.where('name LIKE(?)', "%#{params[:keyword]}%")
   end
 
 private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
   def product_params
     params.require(:product).permit(
       :name,
