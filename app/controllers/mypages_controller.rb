fb1442c56f1728f1fcad068ced94d8677e2f9aa3
taka321 <takasaki1232@gmail.com>
2019-08-19 16:32:24 +0900

    fix

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -8,8 +8,6 @@ class MypagesController < ApplicationController
   end
   
   def identification
-    @profile = Profile.find_by(user_id: current_user)
-    @address = Address.find_by(user_id: current_user)
   end
 
   def edition
