3a4c4fca475d9d12192f6f5ba8885fbd0c28c59c
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-20 18:36:28 +0900

    fix step5.js

--- app/assets/javascripts/step5.js
+++ app/assets/javascripts/step5.js
@@ -1,33 +1,48 @@
 $(document).on('turbolinks:load', function() {
+  $.validator.addMethod("selectCheck", function(value, element, arg){
+    return arg !== value;
+   }, "Value must not equal arg.");
+
   $('.forth-main-wrapper__box').validate({
   rules: {
       number:{
         required: true,
+        number: true,
+        minlength : 14,
         maxlength : 16
       },
-      exp_month:{required: true},
-      exp_month:{required: true},
+      // exp_month: {
+      //   required: true,
+      //   valueNotEquals: "---"
+      // },
+      // exp_month: {required: true},
       cvc: {
         required: true,
+        number: true,
         minlength : 3,
         maxlength : 4
       }
   },
   messages: {
       number: {
-        required: 'これは必須項目です!',
-        maxlength: '最大で16桁です'
+        required: 'これは必須項目です。',
+        number: '数字のみ入力できます。',
+        maxlength: '14~16桁で入力してください。',
+        minlength : '14~16桁で入力してください。',
+        creditcard: 'クレジットカードの番号を入力してください。'
       },
-      exp_month: {
-        required: 'これは必須項目です!'
-      },
-      exp_year: {
-        required: 'これは必須項目です!'
-      },
+      // exp_month: {
+      //   required: 'これは必須項目です。',
+      //   valueNotEquals: '項目を洗濯してください'
+      // },
+      // exp_year: {
+      //   required: 'これは必須項目です。'
+      // },
       cvc: {
-        required: 'これは必須項目です!',
-        minlength : '3桁もしくは4桁の番号です!',
-        maxlength : '3桁もしくは4桁の番号です!'
+        required: 'これは必須項目です。',
+        number: '数字のみ入力できます。',
+        minlength : '3桁もしくは4桁の番号です。',
+        maxlength : '3桁もしくは4桁の番号です。'
       }
   }
 });})
