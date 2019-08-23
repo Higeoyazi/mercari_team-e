e7264956c348f4da0971d5c434e93368f6464bfe
tfljliar <tfljliar1101@gmail.com>
2019-08-23 18:07:39 +0900

    fix new&edit view

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load',function(){
-    var input = $(".input-price").val();
+    var input = $("#product-price").val();
     product_price = parseInt(input);
     if (product_price >= 300 && product_price <= 9999999){
       var fee = Math.floor(product_price * 0.1);
@@ -10,9 +10,9 @@ $(document).on('turbolinks:load',function(){
       $(".sell-price__form__lists__fee__right").html("-");
       $(".sell-price__form__lists__benefit__right").html("-"); 
     }
-  $('.input-price').on('keyup', function(e){
+  $('#product-price').on('keyup', function(e){
     e.preventDefault();
-    var input = $(".input-price").val();
+    var input = $("#product-price").val();
     product_price = parseInt(input);
     if (product_price >= 300 && product_price <= 9999999){
       var fee = Math.floor(product_price * 0.1);
