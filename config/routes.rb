6278103df88fd40643ea3a14ad6f324f829a31f1
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-23 16:20:12 +0900

    add route products to products/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'products#index'
+  get 'products', to: 'products#new'
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
