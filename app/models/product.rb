b5f1a0aada8ff2c0a46f51c369b68cea2d526163
taka321 <takasaki1232@gmail.com>
2019-08-24 12:30:38 +0900

    comment out validate

--- app/models/product.rb
+++ app/models/product.rb
@@ -37,5 +37,5 @@ class Product < ApplicationRecord
                                 
   validates :category_id,       presence: true
   validates :user_id,           presence: true
-  validates :product_images,    presence: true #この書き方でproduct_imagesをproductと一緒にbuildした時のproductのparamsの一つであるproduct_imagesのattributesがあるかどうかvalidしてくれる
+  # validates :product_images,    presence: true #この書き方でproduct_imagesをproductと一緒にbuildした時のproductのparamsの一つであるproduct_imagesのattributesがあるかどうかvalidしてくれる
 end
