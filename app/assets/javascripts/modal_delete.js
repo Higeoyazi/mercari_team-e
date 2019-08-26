7c4b9f8007965067848eb83f8e91f4927c0f903c
okamoto ryo <j270549a@gmail.com>
2019-08-27 04:45:49 +0900

    edit_products_destroy
    商品削除機能について、モーダルウィンドォを使って、Ajaxで削除できるようにしました。

---
+++ app/assets/javascripts/modal_delete.js
@@ -0,0 +1,21 @@
+// モーダルウィンドゥを使って商品削除する処理
+$(function() {
+  $(".delete-btn").on("click",function(){
+    $('#overlay').fadeIn();
+
+    $(document).on("click","#modal-close-btn",function(){
+      $('#overlay').fadeOut();
+    });
+
+    $(document).on("click","#delete-comformation-btn",function(){
+      let ProductID = $(this).attr("data-product.id");
+
+      $.ajax({
+        url: "/products/" + ProductID,
+        type: "POST",
+        data: {"id":ProductID,"_method": "DELETE"} 
+      }) 
+
+    })
+  });
+});
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,6 +66,7 @@ class ProductsController < ApplicationController
 
 
   def destroy
+    binding.pry
     @product.destroy
     flash[:notice] = "削除しました"
     redirect_to root_path
