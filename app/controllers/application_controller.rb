f5cb778249908c4ba6e0877766ba789af2b4e603
taka321 <takasaki1232@gmail.com>
2019-08-29 13:17:24 +0900

    comment out basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
  
   require "payjp"
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth, if: :production?
+  # before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
   def configure_permitted_parameters
@@ -31,13 +31,13 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def production?
-    Rails.env.production?
-  end
+  # def production?
+  #   Rails.env.production?
+  # end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
 end
